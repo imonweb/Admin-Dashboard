aab13c674251c1f03ef4c9314280eb9026ee4aac
imonweb <contact@imonweb.co.uk>
2021-10-08 23:10:54 +0100

    cards

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+// MenuToggle
+let toggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+let main = document.querySelector('.main');
+
+toggle.onclick = function(){
+  navigation.classList.toggle('active');
+  main.classList.toggle('active');
+}
+
+// add hovered class in selected list item
 let list = document.querySelectorAll('.navigation li');
 function activeLink(){
   list.forEach((item) => 
